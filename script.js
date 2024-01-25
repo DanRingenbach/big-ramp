f00caf7551d366386557b95ff2408d7541296a1e
Dan Ringenbach <danringenbach.art@gmail.com>
2024-01-24 19:09:02 -0500

    pdf

--- script.js
+++ script.js
@@ -2,20 +2,19 @@ $(document).ready(function () {
 
     $('#overlay').hide();
 
-    // Click event for main images
     $('.gallery-img').click(function () {
-      var imgSrc = $(this).attr('src');
-      $('#overlayImg').attr('src', imgSrc);
-      $('#overlay').fadeIn();
-    });
-
-    // Click event for overlay to close it
-    $('#overlay').click(function () {
-      $(this).fadeOut();
-    });
-
-    function openPdfOverlay(pdfSrc) {
-        $('#overlayContent').html('<iframe src="' + pdfSrc + '" width="100%" height="100%" style="border: none;"></iframe>');
+        var imgSrc = $(this).attr('src');
+        $('#overlayImg').attr('src', imgSrc);
         $('#overlay').fadeIn();
-      }
+      });
+
+      // Click event for overlay to close it
+      $('#overlay').click(function () {
+        $(this).fadeOut();
+      });
+
   });
+
+  function openPdfInNewTab(pdfSrc) {
+    window.open(pdfSrc, '_blank');
+  }
